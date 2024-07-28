f59b04e1183dec52a7101b7b3f651a65d5176263
matinDesai <matindesai7774@gmail.com>
2024-07-28 20:43:05 +0530

    Himanshu project

--- src/test/java/Execution/RunnerClass.java
+++ src/test/java/Execution/RunnerClass.java
@@ -8,8 +8,8 @@ import io.cucumber.testng.CucumberOptions;
 		features = {"src\\test\\resources\\FeatureFiles\\RahulShettyApplication.feature"},
 		
 		publish = true,
-		
-		plugin = {"pretty"}
+
+		plugin = {"pretty","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
 		
 		
 )
